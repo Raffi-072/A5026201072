d194fb1056d73b68b49a41979e2f88efdc1c10a4
Raffi-072 <Priyadiantama98@gmail.com>
2021-11-25 19:49:46 +0700

    Update tambah.blade.php

--- resources/views/tugas/tambah.blade.php
+++ resources/views/tugas/tambah.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
+	<title>Tugas CRUD-Tugas</title>
 </head>
 <body>
 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Data Tugas</h3>
+	<h2>Data Tugas</h2>
 
 	<a href="/tugas"> Kembali</a>
 
@@ -16,9 +15,9 @@
 	<form action="/tugas/store" method="post">
 		{{ csrf_field() }}
 		IDPegawai <input type="number" name="id_pegawai" required="required"> <br/>
-		Tanggal <input type="date" name="tanggal" required="required"> <br/>
+		Tanggal <input type="datetime-local" name="tanggal" required="required"> <br/>
 		NamaTugas <input type="text" name="nama_tugas" required="required"> <br/>
-		Status <input type="text" name="status" required="required"><br/>
+		Status <input type="text" name="status" required="required" pattern="[A-Za-z]{1}" placeholder="Y/N"><br/>
 		<input type="submit" value="Simpan Data">
 	</form>
 
