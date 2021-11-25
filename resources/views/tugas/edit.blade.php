64fea64a1002d4cc698ebe0c4c2ea998760ff69d
Raffi-072 <Priyadiantama98@gmail.com>
2021-11-25 19:49:01 +0700

    Update edit.blade.php

--- resources/views/tugas/edit.blade.php
+++ resources/views/tugas/edit.blade.php
@@ -1,12 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
+	<title>Tugas CRUD-Tugas</title>
 </head>
 <body>
 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Edit Tugas</h3>
+	<h2>Edit Tugas</h2>
 
 	<a href="/tugas"> Kembali</a>
 
@@ -18,9 +17,9 @@
 		{{ csrf_field() }}
 		<input type="hidden" name="id" value="{{ $t->ID }}"> <br/>
 		IDPegawai <input type="number" name="id_pegawai" required="required" value="{{ $t->IDPegawai }}"> <br/>
-		Tanggal <input type="date" name="tanggal" required="required"value="{{ $t->Tanggal }}"> <br/>
+		Tanggal <input type="datetime-local" name="tanggal" required="required"value="{{ $t->Tanggal }}"> <br/>
 		NamaTugas <input type="text" name="nama_tugas" required="required" value="{{ $t->NamaTugas }}"> <br/>
-		Status <input type="text" name="status" required="required" value="{{ $t->Status }}"><br/>
+		Status <input type="text" name="status" required="required" pattern="[A-Za-z]{1}" placeholder="Y/N" value="{{ $t->Status }}"><br/>
 		<input type="submit" value="Simpan Data">
 	</form>
 	@endforeach
