0b3be5501d9e741ea9bb98b725f221928dbca7a4
Raffi-072 <Priyadiantama98@gmail.com>
2021-12-08 09:50:15 +0700

    Update edit.blade.php

--- resources/views/tugas/edit.blade.php
+++ resources/views/tugas/edit.blade.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tugas CRUD-Query Builder</title>
-</head>
-<body>
-    <h2>Tugas Query Builder</h2>
-	<h2>Edit Tugas</h2>
+@extends('layout.bahagia')
+
+@section('title','Tugas CRUD-Query Builder')
+@section('judulhalaman','Edit Tugas')
+
+@section('konten')
 
 	<a href="/tugas"> Kembali</a>
 
@@ -17,7 +15,28 @@
 		{{ csrf_field() }}
 		<input type="hidden" name="id" value="{{ $t->ID }}"> <br/>
 		IDPegawai <input type="number" name="id_pegawai" required="required" value="{{ $t->IDPegawai }}"> <br/>
-		Tanggal <input type="datetime-local" name="tanggal" required="required"value="{{ $t->Tanggal }}"> <br/>
+
+        <div class="form-group">
+            <label for="dtpickerdemo" class="col-sm-2 control-label">Tanggal :</label>
+            <div class='col-sm-4 input-group date ' id='dtpickerdemo'>
+                <input type='text' class="form-control" name="tanggal" value="{{ $t->Tanggal }}"
+                    required="required" />
+                <span class="input-group-addon">
+                    <span class="glyphicon glyphicon-calendar"></span>
+                </span>
+            </div>
+        </div>
+        <script type="text/javascript">
+            $(function() {
+                $('#dtpickerdemo').datetimepicker({
+                    format: 'YYYY-MM-DD hh:mm:ss',
+                    showTodayButton: false,
+                    locale: 'id',
+                    "defaultDate": new Date(),
+                });
+            });
+        </script>
+
 		NamaTugas <input type="text" name="nama_tugas" required="required" value="{{ $t->NamaTugas }}"> <br/>
 		Status <input type="text" name="status" required="required" pattern="[A-Za-z]{1}" placeholder="Y/N" value="{{ $t->Status }}"><br/>
 		<input type="submit" value="Simpan Data">
@@ -26,4 +45,5 @@
 
 
 </body>
-</html>
+
+@endsection
