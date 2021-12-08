2b795996c85792a15f64def8fc6b4d81fcddd90e
Raffi-072 <Priyadiantama98@gmail.com>
2021-12-08 09:49:57 +0700

    Update tambah.blade.php

--- resources/views/tugas/tambah.blade.php
+++ resources/views/tugas/tambah.blade.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tugas CRUD-Query Builder</title>
-</head>
-<body>
-    <h2>Tugas Query Builder</h2>
-	<h2>Data Tugas</h2>
+@extends('layout.bahagia')
+
+@section('title','Tugas CRUD-Query Builder')
+@section('judulhalaman','Menambah Data Tugas')
+
+@section('konten')
 
 	<a href="/tugas"> Kembali</a>
 
@@ -15,11 +13,31 @@
 	<form action="/tugas/store" method="post">
 		{{ csrf_field() }}
 		IDPegawai <input type="number" name="id_pegawai" required="required"> <br/>
-		Tanggal <input type="datetime-local" name="tanggal" required="required"> <br/>
+        <div class="form-group">
+            <label for="dtpickerdemo" class="col-sm-2 control-label">Tanggal :</label>
+            <div class='col-sm-4 input-group date ' id='dtpickerdemo'>
+                <input type='text' class="form-control" name="tanggal" required="required"/>
+                <span class="input-group-addon">
+                    <span class="glyphicon glyphicon-calendar"></span>
+                </span>
+            </div>
+        </div>
+        <script type="text/javascript">
+            $(function() {
+                $('#dtpickerdemo').datetimepicker({
+                    format: 'YYYY-MM-DD hh:mm:ss',
+                    showTodayButton: false,
+                    locale: 'id',
+                    "defaultDate": new Date(),
+                });
+            });
+        </script>
+
 		NamaTugas <input type="text" name="nama_tugas" required="required"> <br/>
 		Status <input type="text" name="status" required="required" pattern="[A-Za-z]{1}" placeholder="Y/N"><br/>
 		<input type="submit" value="Simpan Data">
 	</form>
 
 </body>
-</html>
+
+@endsection
