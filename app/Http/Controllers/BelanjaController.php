53de57d9b2bda6750742229b5e1fcf6543e96598
Raffi-072 <Priyadiantama98@gmail.com>
2021-12-15 13:34:56 +0700

    EAS_PWEB-A_D4

--- app/Http/Controllers/BelanjaController.php
+++ app/Http/Controllers/BelanjaController.php
@@ -22,28 +22,27 @@ class BelanjaController extends Controller
         return view('keranjangbelanja.tambah');
     }
 
-    // method untuk insert data ke table agen
+
     public function store(Request $request)
     {
-        // insert data ke table agen
+
         DB::table('keranjangbelanja')->insert([
             'KodeBarang' => $request->kodebarang,
             'Jumlah' => $request->jumlah,
             'Harga' => $request->harga,
         ]);
-        // alihkan halaman ke halaman agen
+
         return redirect('/keranjangbelanja');
     }
 
 
-    // method untuk hapus data agen
+
     public function hapus($id)
     {
-        // menghapus data agen berdasarkan id yang dipilih
+
         DB::table('keranjangbelanja')->where('ID', $id)->delete();
 
-        // alihkan halaman ke halaman agen
+
         return redirect('/keranjangbelanja');
     }
-
 }
--- resources/views/keranjangbelanja/tambah.blade.php
+++ resources/views/keranjangbelanja/tambah.blade.php
@@ -5,22 +5,25 @@
 
 @section('konten')
 
-	<br/>
-	<br/>
+    <br />
+    <br />
 
-	<form action="/keranjangbelanja/store" method="post">
-		{{ csrf_field() }}
+    <form action="/keranjangbelanja/store" method="post">
+        {{ csrf_field() }}
         <div class="form-group">
-            <label for = "kodebarang">Kode Barang</label> <input type="text" name="kodebarang" required="required" class = "form-control"> <br/>
+            <label for="kodebarang">Kode Barang</label> <input type="text" name="kodebarang" required="required"
+                class="form-control"> <br />
 
-            <label for = "jumlah">Jumlah Pembelian </label> <input type="text" name="jumlah" required="required" class = "form-control"> <br/>
+            <label for="jumlah">Jumlah Pembelian </label> <input type="text" name="jumlah" required="required"
+                class="form-control"> <br />
 
 
-            <label for = "harga">Harga Per item </label> <input type="text" name="harga" required="required" class = "form-control"> <br/>
+            <label for="harga">Harga Per item </label> <input type="text" name="harga" required="required"
+                class="form-control"> <br />
 
             <br><br>
-            <input type="submit" class="btn btn-success"  value="Simpan Data">
-            <a href="/keranjangbelanja" class="btn btn-danger" > Kembali</a>
+            <input type="submit" class="btn btn-success" value="Simpan Data">
+            <a href="/keranjangbelanja" class="btn btn-danger"> Kembali</a>
         </div>
     </form>
 
